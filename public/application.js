686814d7cfeddb5a00d0ef336e3292b71bfcc1da
Matthew Andrews <matt@mattandre.ws>
2014-08-30 14:53:35 +0100

    Implement offline-first IndexedDB logic

--- public/application.js
+++ public/application.js
@@ -1,7 +1,7 @@
 (function() {
   var api = 'http' + (location.hostname === 'localhost' ? '://localhost:3000' : 's://offline-news-api.herokuapp.com') + '/stories';
-
-  var ul, h1;
+  var synchronizeInProgress;
+  var db, ul, h1;
 
   databaseOpen()
     .then(function() {
@@ -9,7 +9,9 @@
       document.body.addEventListener('click', onClick);
       window.addEventListener('popstate', refreshView);
     })
-    .then(refreshView);
+    .then(databaseStoriesGet)
+    .then(renderAllStories)
+    .then(synchronize);
 
   function onClick(e) {
     if (e.target.classList.contains('js-link')) {
@@ -23,10 +25,10 @@
     var guid = (location.pathname+location.search).substring(1);
     if (guid === '') {
       renderAllStories();
-      return serverStoriesGet().then(renderAllStories);
+      return databaseStoriesGet().then(renderAllStories);
     }
     renderOneStory();
-    return serverStoriesGet(guid).then(renderOneStory);
+    return databaseStoriesGetById(guid).then(renderOneStory);
   }
 
   function renderAllStories(stories) {
@@ -43,6 +45,42 @@
     main.innerHTML = '<h1>'+story.title+'</h1>'+story.body;
   }
 
+  function synchronize() {
+    if (synchronizeInProgress) return synchronizeInProgress;
+    synchronizeInProgress = Promise.all([serverStoriesGet(), databaseStoriesGet()])
+      .then(function(results) {
+        var promises = [];
+        var remoteStories = results[0];
+        var localStories = results[1];
+
+        // Add new stories downloaded from server to the database
+        promises = promises.concat(remoteStories.map(function(story) {
+          if (!arrayContainsStory(localStories, story)) {
+            return databaseStoriesPut(story);
+          }
+        }));
+
+        // Delete stories that are no longer on the server from the database
+        promises = promises.concat(localStories.map(function(story) {
+          if (!arrayContainsStory(remoteStories, story)) {
+            return databaseStoriesDelete(story);
+          }
+        }));
+
+        return promises;
+      })
+      .then(function() {
+        synchronizeInProgress = undefined;
+      })
+      .then(refreshView);
+  }
+
+  function arrayContainsStory(array, story) {
+    return array.some(function(arrayStory) {
+      return arrayStory.guid === story.guid;
+    });
+  }
+
   function databaseOpen() {
     return new Promise(function(resolve, reject) {
       var version = 1;
@@ -50,7 +88,7 @@
       request.onupgradeneeded = function(e) {
         db = e.target.result;
         e.target.transaction.onerror = reject;
-        db.createObjectStore('news', { keyPath: 'guid' });
+        db.createObjectStore('stories', { keyPath: 'guid' });
       };
       request.onsuccess = function(e) {
         db = e.target.result;
@@ -60,8 +98,61 @@
     });
   }
 
-  function serverStoriesGet(guid) {
+  function databaseStoriesPut(story) {
     return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.put(story);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function databaseStoriesGet() {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+
+      var data = [];
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+        if (result) {
+          data.push(result.value);
+          result.continue();
+        } else {
+          resolve(data);
+        }
+      };
+    });
+  }
+
+  function databaseStoriesGetById(guid) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.get(guid);
+      request.onsuccess = function(e) {
+        var result = e.target.result;
+        resolve(result);
+      };
+      request.onerror = reject;
+    });
+  }
+
+  function databaseStoriesDelete(story) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.delete(story.guid);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function serverStoriesGet(guid) { return new Promise(function(resolve, reject) {
       superagent.get(api+'/' + (guid ? guid : ''))
         .end(function(err, res) {
           if (!err && res.ok) resolve(res.body);
