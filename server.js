16fb93777f7c062739e3a955c5e39fc91696b72d
Matthew Andrews <matt@mattandre.ws>
2014-08-30 13:41:51 +0100

    Support subdirectories

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname+'/public'));
 
 app.get(/^\//, function(req, res) {
-  res.sendfile('public/index.html');
+  res.sendFile('public/index.html', { root: __dirname });
 });
 
 app.listen(8080);
