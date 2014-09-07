376bc6f52e02333b0bf08d3c3aef2f38c6097008
Matt Andrews <matt@mattandre.ws>
2014-09-07 12:26:10 +0100

    DB Namespace

--- public/application.js
+++ public/application.js
@@ -87,7 +87,7 @@
   function databaseOpen() {
     return new Promise(function(resolve, reject) {
       var version = 1;
-      var request = indexedDB.open('news-server-rendered', version);
+      var request = indexedDB.open('offline-news-server-rendered', version);
       request.onupgradeneeded = function(e) {
         db = e.target.result;
         e.target.transaction.onerror = reject;
